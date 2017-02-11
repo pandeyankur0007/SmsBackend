62e37bc8c94adf74491f60cfc595ce1637e9fded
pandeyankur0007 <pandeyankur0007@gmail.com>
2017-02-12 01:37:25 +0530

    Adding heroku setting

--- src/main/java/SmsBackend.java
+++ src/main/java/SmsBackend.java
@@ -1,17 +1,26 @@
 import com.twilio.sdk.TwilioRestClient;
 import com.twilio.sdk.resource.instance.Sms;
+import spark.Spark;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static spark.Spark.*;
+import static spark.Spark.post;
 
 /**
  * Created by ankur on 9/2/17.
  */
 public class SmsBackend {
     public static void main(String[] args) {
-
+//Heroku assigns different port each time, hence reading it from process.
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+        Spark.port(port);
         TwilioRestClient client = new TwilioRestClient("AC7c44c7fce4d3faa3e5c38575c1831ac0",
                 "21f6597479968c26c22f6482a2ea6bb2");
 
